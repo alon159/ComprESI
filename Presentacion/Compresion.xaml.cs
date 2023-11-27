4640d56c56b891a785628ef485a8531a15631d1f
alon159 <aloncrespfer@hotmail.com>
2023-11-28 00:09:35 +0100

    Mejoras

--- Presentacion/Compresion.xaml.cs
+++ Presentacion/Compresion.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.ColorSpaces;
+using SixLabors.ImageSharp.Processing;
 
 namespace ComprESI.Presentacion
 {
@@ -20,12 +26,24 @@ namespace ComprESI.Presentacion
     /// </summary>
     public partial class Compresion : Page
     {
+        public string rutaArchivo;
+
         public Compresion()
         {
             InitializeComponent();
+            rutaArchivo="";
+            gridDrop.AllowDrop = true;
+            bttnGuardar.IsEnabled = false;
+            listAlgoritmos.IsEnabled = false;
+            txtFactor.IsEnabled = false;
+            var metodos= new List<string> { "Bicubic", "Box", "CatmullRom","Hermite", 
+                "Lanczos2","Lanczos3","Lanczos5","Lanczos8","MitchellNetravali","NearestNeighbor",
+                "Robidoux","RobidouxSharp","Spline","Triangle", "Welch"};
+            listAlgoritmos.ItemsSource = metodos;
+            listAlgoritmos.SelectedIndex = 0;
         }
 
-        private void Grid_Drop(object sender, DragEventArgs e)
+        private void gridDrop_Drop(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
@@ -33,10 +51,45 @@ namespace ComprESI.Presentacion
 
                 if (archivos != null && archivos.Length > 0)
                 {
-                    string rutaArchivo = archivos[0]; // Tomamos solo el primer archivo en este ejemplo
-
-                    MessageBox.Show("Archivo seleccionado: " + rutaArchivo);
+                    rutaArchivo = archivos[0]; // Tomamos solo el primer archivo en este ejemplo
                 }
+            }
+            gridDrop.AllowDrop = false;
+            imgDrop.Source = new BitmapImage(new Uri(rutaArchivo));
+            bttnGuardar.IsEnabled = true;
+            listAlgoritmos.IsEnabled = true;
+            txtFactor.IsEnabled = true;
+            
+        }
+
+        private void bttnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            Int32 factor = 0;
+            try
+            {
+                factor = Int32.Parse(txtFactor.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            using (SixLabors.ImageSharp.Image image = SixLabors.ImageSharp.Image.Load(rutaArchivo))
+            {
+                int width = image.Width / factor;
+                int height = image.Height / factor;
+                image.Mutate(x => x.Resize(width, height));
+
+                //cuadro para guardar archivo
+                Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+                //como obtener el nombre del archivo desde la ruta
+                string[] ruta = rutaArchivo.Split('\\');
+                string[] nombre = ruta[ruta.Length - 1].Split('.');
+                dlg.FileName = nombre[0]+"_compressed"; // Nombre por defecto
+                dlg.DefaultExt = "." + nombre[1]; // Extensión por defecto
+                dlg.Title = "Guardar imagen comprimida"; // Título de la ventana
+                dlg.Filter = "Archivos de imagen|*.jpg;*.jpeg;*.png;*.bmp"; // Filtro de archivos
+                dlg.ShowDialog(); // Mostrar ventana
+                image.Save(dlg.FileName); // Guardar imagen
             }
         }
     }
